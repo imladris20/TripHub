55d060f7e75fdeffa5e2c8fbd608eea59683a1c8
Po Lien Lin <polien.joe.lin@gmail.com>
2023-11-16 19:07:18 +0800

    Practicing zustand and react-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 // import { Route, Routes } from "react-router-dom";
-// import { useContext } from "react";
-// import Context from "./context/ContextTemplate";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import useStore from "./store/store";
 
 function App() {
-  // const {
-  //   state, function1, function2
-  // } = useContext(Context);
+  const { bears, increasePopulation, removeAllBears } = useStore();
+
+  const wait = (ms) => {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  };
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["getPost"],
+    queryFn: async () => {
+      await wait(3000);
+      const { data } = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts/1",
+      );
+      return data;
+    },
+  });
 
   return (
     <>
@@ -16,6 +32,24 @@ function App() {
       <p className="cursor-pointer font-mono text-2xl text-gray-500">
         Start Your Project
       </p>
+      <div className="mt-5 border border-solid border-gray-300 p-2">
+        {isLoading ? "正在axios喔" : JSON.stringify(data)}
+      </div>
+      <div className="mt-5 border border-solid border-gray-300 p-2">
+        <h2>Bears: {bears}</h2>
+        <button
+          className="mr-4 h-8 w-16 cursor-pointer bg-green-200 p-1"
+          onClick={increasePopulation}
+        >
+          +1熊
+        </button>
+        <button
+          className="mr-4 h-8 w-16 cursor-pointer bg-gray-200 p-1"
+          onClick={removeAllBears}
+        >
+          歸零
+        </button>
+      </div>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import { ContextProvider } from "./context/ContextTemplate.jsx";
+// import { ContextProvider } from "./context/ContextTemplate.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ContextProvider>
+      <QueryClientProvider client={queryClient}>
         <App />
-      </ContextProvider>
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
