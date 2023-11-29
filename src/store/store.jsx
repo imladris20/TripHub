5944e1f2f3b0c1969aec82d12171ae5c5882cf03
Po Lien Lin <polien.joe.lin@gmail.com>
2023-11-29 09:41:25 +0800

    Change order of states and delete unused ones

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -6,17 +6,8 @@ const mapId = import.meta.env.VITE_GOOGLE_MAPS_ID;
 const TAIWAN = { lat: 23.553118, lng: 121.0211024 };
 
 const useStore = create((set, get) => ({
-  open: false,
-  currentCenter: TAIWAN,
-  currentZoom: 8.5,
   apiKey,
   mapId,
-  selected: null,
-  placeResult: null,
-  isLogin: false,
-  isSignWindowOpen: false,
-  database: null,
-  searchItemDetailInfo: null,
   typeOptions: [
     "自然景點",
     "人文景點",
@@ -38,55 +29,8 @@ const useStore = create((set, get) => ({
     "拉麵",
     "慈善機構",
   ],
-  setSearchItemDetailInfo: (place) => {
-    set(
-      produce((state) => {
-        state.searchItemDetailInfo = place;
-      }),
-    );
-  },
-  setIsLogin: (boolean) => {
-    set(
-      produce((state) => {
-        state.isLogin = boolean;
-      }),
-    );
-  },
-  setDatabase: (db) => {
-    set(
-      produce((state) => {
-        state.database = db;
-      }),
-    );
-  },
-  setIsSignWindowOpen: (boolean) => {
-    set(
-      produce((state) => {
-        state.isSignWindowOpen = boolean;
-      }),
-    );
-  },
-  setPlaceResult: (placeResult) => {
-    set(
-      produce((state) => {
-        state.placeResult = placeResult;
-      }),
-    );
-  },
-  setSelected: (placeInfo) => {
-    set(
-      produce((state) => {
-        state.selected = placeInfo;
-      }),
-    );
-  },
-  setMap: (newMap) => {
-    set(
-      produce((state) => {
-        state.map = newMap;
-      }),
-    );
-  },
+  currentCenter: TAIWAN,
+  currentZoom: 8.5,
   setCurrentCenter: (geolocation) => {
     set(
       produce((state) => {
@@ -98,6 +42,54 @@ const useStore = create((set, get) => ({
     set(
       produce((state) => {
         state.currentZoom = level;
+      }),
+    );
+  },
+  selected: null,
+  setSelected: (placeInfo) => {
+    set(
+      produce((state) => {
+        state.selected = placeInfo;
+      }),
+    );
+  },
+  placeResult: null,
+  setPlaceResult: (placeResult) => {
+    set(
+      produce((state) => {
+        state.placeResult = placeResult;
+      }),
+    );
+  },
+  isLogin: false,
+  setIsLogin: (boolean) => {
+    set(
+      produce((state) => {
+        state.isLogin = boolean;
+      }),
+    );
+  },
+  isSignWindowOpen: false,
+  setIsSignWindowOpen: (boolean) => {
+    set(
+      produce((state) => {
+        state.isSignWindowOpen = boolean;
+      }),
+    );
+  },
+  database: null,
+  setDatabase: (db) => {
+    set(
+      produce((state) => {
+        state.database = db;
+      }),
+    );
+  },
+  searchItemDetailInfo: null,
+  setSearchItemDetailInfo: (place) => {
+    set(
+      produce((state) => {
+        state.searchItemDetailInfo = place;
       }),
     );
   },
