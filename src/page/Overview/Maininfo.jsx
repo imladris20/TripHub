257e9061874dc7856a4d62a0184751cc6de206b3
Po Lien Lin <polien.joe.lin@gmail.com>
2023-12-18 10:58:01 +0800

    Fix qrcode link

--- src/page/Overview/Maininfo.jsx
+++ src/page/Overview/Maininfo.jsx
@@ -44,12 +44,13 @@ const Maininfo = ({ trip, tripId }) => {
           <div className="stat-value mt-2">
             <a
               className="whitespace-nowrap text-xl font-bold "
-              // href={`https://triphub-polien-sprint4.web.app/overview/${tripId}`}
-              href={`http://localhost:5173/overview/${tripId}`}
+              href={`https://triphub.web.app/overview/${tripId}`}
+              // href={`http://localhost:5173/overview/${tripId}`}
               target="_blank"
             >
               <QRCodeSVG
-                value={`http://localhost:5173/overview/${tripId}`}
+                // value={`http://localhost:5173/overview/${tripId}`}
+                value={`https://triphub.web.app/overview/${tripId}`}
                 className="h-16 w-16"
               />
             </a>
