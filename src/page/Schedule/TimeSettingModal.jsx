a75c9c70d76f3f58f5bcd942ce59860e56e4be8c
Po Lien Lin <polien.joe.lin@gmail.com>
2023-12-25 16:09:21 +0800

    Refactor calculateEndTime

--- src/page/Schedule/TimeSettingModal.jsx
+++ src/page/Schedule/TimeSettingModal.jsx
@@ -4,7 +4,7 @@ import { useEffect, useRef, useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
 import useStore, { scheduleStore } from "../../store/store";
 import { TimeIcon } from "../../utils/icons";
-import { addDurationToTime } from "../../utils/timeUtil";
+import { addDurationToTime, calculateEndTime } from "../../utils/timeUtil";
 
 const TimeSettingModal = ({
   name,
@@ -243,21 +243,3 @@ const TimeSettingModal = ({
 };
 
 export default TimeSettingModal;
-
-function calculateEndTime(start, hours, minutes) {
-  const startHours = parseInt(start.split(":")[0], 10);
-  const startMinutes = parseInt(start.split(":")[1], 10);
-  const stayHoursNum = parseInt(hours, 10);
-  const stayMinutesNum = parseInt(minutes, 10);
-
-  const endHours = startHours + stayHoursNum;
-  const endMinutes = startMinutes + stayMinutesNum;
-
-  const endTime = new Date(0, 0, 0, endHours, endMinutes);
-
-  const stringifyEndTime =
-    endTime.getHours().toString().padStart(2, "0") +
-    ":" +
-    endTime.getMinutes().toString().padStart(2, "0");
-  return stringifyEndTime;
-}
--- src/utils/timeUtil.js
+++ src/utils/timeUtil.js
@@ -15,3 +15,21 @@ export function addDurationToTime(startTime, duration) {
 
   return result;
 }
+
+export function calculateEndTime(start, hours, minutes) {
+  const startHours = parseInt(start.split(":")[0], 10);
+  const startMinutes = parseInt(start.split(":")[1], 10);
+  const stayHoursNum = parseInt(hours, 10);
+  const stayMinutesNum = parseInt(minutes, 10);
+
+  const endHours = startHours + stayHoursNum;
+  const endMinutes = startMinutes + stayMinutesNum;
+
+  const endTime = new Date(0, 0, 0, endHours, endMinutes);
+
+  const stringifyEndTime =
+    endTime.getHours().toString().padStart(2, "0") +
+    ":" +
+    endTime.getMinutes().toString().padStart(2, "0");
+  return stringifyEndTime;
+}
